1ffdcc58c4d6eba5dd4d6192df48a3d83d0b71d8
Anupa_J <janupa07@gmail.com>
2025-10-12 23:57:33 +0530

    msa

--- src/test/java/com/anupa/configserver/ConfigserverApplicationTests.java
+++ src/test/java/com/anupa/configserver/ConfigserverApplicationTests.java
@@ -1,10 +1,9 @@
-package com.cbt.configserverboot3;
+package com.anupa.configserver;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Configserverboot3ApplicationTests {
+class ConfigserverApplicationTests {
 
     //@Test
     void contextLoads() {
